b673f94409044dbcf7e2cdb914972000ab32af91
milespcorn <124821577+milespcorn@users.noreply.github.com>
2023-04-02 12:33:03 -0400

    Update Serial_LU_Factorization.cpp
    Changed the check to PA = LU.

--- Serial_LU_Factorization.cpp
+++ Serial_LU_Factorization.cpp
@@ -205,9 +205,9 @@ int main()
 		ans[i] = new double[3];
 	}
 	
-	matrixMult(P, L, C, 3);
+	matrixMult(P, A, C, 3);
 
-	matrixMult(C, U, ans, 3);
+	matrixMult(L, U, ans, 3);
 
 	bool sameMatrix = checkAnswer(matrix, ans, 3);
 
