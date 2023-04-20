777ea0fc63dede1cbe78b62a931fc18ae15bea87
unknown <lenysm@rpi.edu>
2023-04-20 10:48:28 -0400

    removed debug statements

--- mpi_io.cpp
+++ mpi_io.cpp
@@ -57,7 +57,6 @@ void write(double* flattened_matrix, int num_rows, int num_cols, MPI_File fh,
                  MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
     }
     MPI_Status status;
-    cout << "me with\n";
     MPI_File_write_at_all(fh, rank * doubles_per_rank * sizeof(double),
                           flattened_matrix, doubles_per_rank, MPI_DOUBLE,
                           &status);
@@ -68,20 +67,16 @@ void write(double* flattened_matrix, int num_rows, int num_cols, MPI_File fh,
     MPI_Status status;
     MPI_Recv(buf, doubles_per_rank + (num_doubles % doubles_per_rank),
              MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
-    cout << "da\n";
     MPI_File_write_at_all(fh, rank * doubles_per_rank * sizeof(double), buf,
                           doubles_per_rank + (num_doubles % doubles_per_rank),
                           MPI_DOUBLE, &status);
-    cout << "unstuck\n";
     delete[] buf;
   } else {
     double* buf = new double[doubles_per_rank];
     MPI_Status status;
     MPI_Recv(buf, doubles_per_rank, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
-    cout << "bois\n";
     MPI_File_write_at_all(fh, rank * doubles_per_rank * sizeof(double), buf,
                           doubles_per_rank, MPI_DOUBLE, &status);
-    cout << "unstuck\n";
     delete[] buf;
   }
 }
@@ -99,13 +94,11 @@ int main(int argc, char** argv) {
   MPI_Status status;
   int num_doubles = num_cols * num_rows;
   int doubles_per_rank = num_doubles / num_ranks;
-  cout << "ummmmm\n";
 
   if (rank == num_ranks - 1) {
     int num_doubles = num_rows * num_cols;
     int double_per_rank = num_doubles / num_ranks;
     double* buf = new double[double_per_rank + (num_doubles % double_per_rank)];
-    cout << "so\n";
     MPI_File_read_at_all(fh, rank * double_per_rank * sizeof(double), buf,
                          double_per_rank + (num_doubles % double_per_rank),
                          MPI_DOUBLE, &status);
@@ -113,8 +106,6 @@ int main(int argc, char** argv) {
              0, 0, MPI_COMM_WORLD);
     delete[] buf;
 
-    cout << "me\n";
-    cout << "when" << rank << ":" << fh << "\n";
     MPI_File_close(&fh);
     MPI_File_open(MPI_COMM_WORLD, "L", MPI_MODE_WRONLY | MPI_MODE_CREATE, MPI_INFO_NULL,&fh);
     write(nullptr, num_rows, num_cols, fh, num_ranks, doubles_per_rank,rank);
@@ -127,17 +118,14 @@ int main(int argc, char** argv) {
     MPI_File_open(MPI_COMM_WORLD, "P", MPI_MODE_WRONLY | MPI_MODE_CREATE, MPI_INFO_NULL,&fh);
     write(nullptr, num_rows, num_cols, fh, num_ranks, doubles_per_rank,rank);
 
-    cout << "here" << rank << "\n";
   } else if (rank != 0) {
     int num_doubles = num_rows * num_cols;
     int double_per_rank = num_doubles / num_ranks;
     double* buf = new double[double_per_rank];
-    cout << "like\n";
     MPI_File_read_at_all(fh, rank * double_per_rank * sizeof(double), buf,
                          double_per_rank, MPI_DOUBLE, &status);
     MPI_Send(buf, double_per_rank, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
     delete[] buf;
-    cout << "when" << rank << ":" << fh << "\n";
     
     MPI_File_close(&fh);
     MPI_File_open(MPI_COMM_WORLD, "L", MPI_MODE_WRONLY | MPI_MODE_CREATE, MPI_INFO_NULL,&fh);
@@ -150,21 +138,14 @@ int main(int argc, char** argv) {
     MPI_File_close(&fh);
     MPI_File_open(MPI_COMM_WORLD, "P", MPI_MODE_WRONLY | MPI_MODE_CREATE, MPI_INFO_NULL,&fh);
     write(nullptr, num_rows, num_cols, fh, num_ranks, doubles_per_rank,rank);
-
-
-
-    cout << "here" << rank << "\n";
-
     
   } else {
     double* flattened_matrix;
     matrix_cuda_alloc(&flattened_matrix, num_cols);
     int double_per_rank = num_doubles / num_ranks;
-    cout << "wtf\n";
     MPI_File_read_at_all(fh, rank * double_per_rank * sizeof(double),
                          flattened_matrix, double_per_rank, MPI_DOUBLE,
                          &status);
-    cout << "when" << rank << ":" << fh << "\n";
 
     for (int i = 1; i < num_ranks; ++i) {
       if (i == num_ranks - 1)
@@ -195,11 +176,9 @@ int main(int argc, char** argv) {
     MPI_File_open(MPI_COMM_WORLD, "P", MPI_MODE_WRONLY | MPI_MODE_CREATE, MPI_INFO_NULL,&fh);
     write(P, num_rows, num_cols, fh, num_ranks, doubles_per_rank,rank);
     
-    cout << "bruh\n";
     
     matrix_cuda_free(&flattened_matrix, &L, &U, &P);
   }
-  cout << "bye " << rank << "\n";
   MPI_Finalize();
 
   return 0;
